c78ff0b6823360f46782c5a99605a94fcabb64e0
Daniel Guilherme Dias Carvalho <daniel.gdc22@gmail.com>
2021-08-19 12:52:33 -0300

    First Commit , criacao de controladores

---
+++ SalesMVC/Models/Department.cs
@@ -0,0 +1,15 @@
+﻿namespace SalesMVC.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string DeptName { get; set; }
+
+        public Department(int id, string deptName)
+        {
+            Id = id;
+            DeptName = deptName;
+
+        }
+    }
+}
