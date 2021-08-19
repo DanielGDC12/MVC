ee0907f0c3087a70e804c7baa6be8dd7e52ad9a6
Daniel Guilherme Dias Carvalho <daniel.gdc22@gmail.com>
2021-08-19 18:05:37 -0300

    Criacao das outras classes do negocio instanciacao de DBSET e add no banco com migrations

---
+++ SalesMVC/Models/Department.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace SalesMVC.Models
+{
+    public class Department
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+
+        public  Department(int id , string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+    }
+}
--- SalesMVC/Program.cs
+++ SalesMVC/Program.cs
@@ -13,9 +13,10 @@ namespace SalesMVC
     {
         public static void Main(string[] args)
         {
+           
             CreateHostBuilder(args).Build().Run();
         }
-
+        
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
